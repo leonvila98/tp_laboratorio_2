1d9a2dbcf496172c401d66740c28c1df768b8c8a
leon <leon.18@live.com.ar>
2020-10-04 20:21:44 -0300

    tp2 funcionando

---
+++ TP2/TP-02-Cascara.zip (Unzipped Files)/TP-02/Entidades/Ciclomotor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color)
+            :base(chasis, marca, color)
+        { }
+
+        /// <summary>
+        /// Las motos son chicas
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Chico;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CICLOMOTOR");
+            sb.AppendLine(this);
+            sb.AppendFormat("TAMAÑO : {0}\n", this.Tamanio);
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
